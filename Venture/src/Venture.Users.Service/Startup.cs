1c35c8a3bd31e1c0a191310484acbafeb0a7e501
Radu Solca <radu.ssolca@gmail.com>
2017-04-20 15:40:43 +0300

    Create Project Add Users Service Add Owin and Nancy to Users Service

---
+++ Venture/src/Venture.Users.Service/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Nancy.Owin;
+
+namespace Venture.Users.Service
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOwin(
+                buildFunc => buildFunc.UseNancy()
+            );
+        }
+    }
+}
---
+++ Venture/src/Venture.Users.Service/UsersModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Venture.Users.Service
+{
+    public sealed class UsersModule : NancyModule
+    {
+        public UsersModule()
+        {
+            Get("/", _ => "Hello World");
+        }
+    }
+}
